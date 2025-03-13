3ba0498cb1cec055168cac64d3660610564cbc6d
Dfrias2022 <diegoarielfrias2020@gmail.com>
2025-03-13 20:44:55 -0300

    nuevos cambios

--- js/script.js
+++ js/script.js
@@ -3,62 +3,82 @@ const productos = [
     {
         nombre: "Aceite natura girasol x900cc",
         imagen: "imagenes/1.jpg",
-        precio: 19.99
+        precio: 2200.00
     },
     {
         nombre: "Aceite marolio girasol x900cc",
         imagen: "imagenes/2.jpg",
-        precio: 29.99
+        precio: 2100.00
     },
     {
         nombre: "Sal fina celusal x500Grs",
         imagen: "imagenes/3.jpg",
-        precio: 39.99
+        precio: 1000.00
     },
     {
         nombre: "Hamburguesa Paty x4",
         imagen: "imagenes/4.jpg",
-        precio: 49.99
+        precio: 4300.00
     },
     {
         nombre: "Galletita media tarde x3",
         imagen: "imagenes/5.jpg",
-        precio: 49.99
+        precio: 1100.00
     },
     {
         nombre: "Galletita Desfile x400Grs",
         imagen: "imagenes/6.jpg",
-        precio: 49.99
+        precio: 1600.00
     },
     {
         nombre: "Hamburguesa Paty finito x2",
         imagen: "imagenes/7.jpg",
-        precio: 49.99
+        precio: 1300.00
     },
     {
         nombre: "Picadillo de carne Molto x 70Grs",
         imagen: "imagenes/8.jpg",
-        precio: 49.99
+        precio: 700.00
     },
     {
         nombre: "Hamburguesa Barfy x4",
         imagen: "imagenes/9.jpg",
-        precio: 49.99
+        precio: 1800.00
     },
     {
         nombre: "Petaca licor cusenier menta",
         imagen: "imagenes/10.jpg",
-        precio: 49.99
+        precio: 2000.00
     },
     {
         nombre: "Galletita La providencia x3",
         imagen: "imagenes/11.jpg",
-        precio: 49.99
+        precio: 1100.00
     },
     {
         nombre: "Papel higienico Duplex x50Mts",
         imagen: "imagenes/12.jpg",
-        precio: 49.99
+        precio: 600.00
+    },
+    {
+        nombre: "Aceite cañuelas 1.5Lts",
+        imagen: "imagenes/13.jpg",
+        precio: 4200.00
+    },
+    {
+        nombre: "Pure molto x520Grs",
+        imagen: "imagenes/14.jpg",
+        precio: 1000.00
+    },
+    {
+        nombre: "Fideo luchetti x500Grs",
+        imagen: "imagenes/15.jpg",
+        precio: 100.00
+    },
+    {
+        nombre: "Azucar marolio x1kg",
+        imagen: "imagenes/16.jpg",
+        precio: 1100.00
     }
 ];
 
