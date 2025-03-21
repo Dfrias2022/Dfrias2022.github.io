e510dec860cc73931da6539548a418ddc2220cfc
Dfrias2022 <diegoarielfrias2020@gmail.com>
2025-03-21 20:02:03 -0300

    nuevo cambio

--- js/script.js
+++ js/script.js
@@ -19,92 +19,92 @@ function actualizarTitulo() {
 // Datos de los productos (pueden venir de una API, base de datos, etc.)
 const productos = [
     {
-        nombre: "Aceite Natura girasol x900cc",
+        nombre: "Aceite Natura girasol x900cc[SIN DISPONIBILIDAD]",
         imagen: "imagenes/1.jpg",
         precio: "$2500.00"
     },
     {
-        nombre: "Aceite Marolio girasol x900cc",
+        nombre: "Aceite Marolio girasol x900cc[SIN DISPONIBILIDAD]",
         imagen: "imagenes/2.jpg",
         precio: "$2300.00"
     },
     {
-        nombre: "Sal fina Celusal x500Grs",
+        nombre: "Sal fina Celusal x500Grs[SIN DISPONIBILIDAD]",
         imagen: "imagenes/3.jpg",
         precio: "$1000.00"
     },
     {
-        nombre: "Hamburguesa clasica ''Paty'' caja x4",
+        nombre: "Hamburguesa clasica ''Paty'' caja x4[SIN DISPONIBILIDAD]",
         imagen: "imagenes/4.jpg",
         precio: ["$4300.00 c/u","x2 $8000.00"]
     },
     {
-        nombre: "Galletita media tarde x3",
+        nombre: "Galletita media tarde x3[SIN DISPONIBILIDAD]",
         imagen: "imagenes/5.jpg",
         precio: "$1100.00"
     },
     {
-        nombre: "Galletita Desfile x400Grs",
+        nombre: "Galletita Desfile x400Grs[SIN DISPONIBILIDAD]",
         imagen: "imagenes/6.jpg",
         precio: "$1600.00"
     },
     {
-        nombre: "Picadillo de carne Molto x 70Grs",
+        nombre: "Picadillo de carne Molto x 70Grs[SIN DISPONIBILIDAD]",
         imagen: "imagenes/8.jpg",
         precio: "$700.00"
     },
     {
-        nombre: "Hamburguesa ''Barfy'' caja x4",
+        nombre: "Hamburguesa ''Barfy'' caja x4[SIN DISPONIBILIDAD]",
         imagen: "imagenes/9.jpg",
         precio: ["$1800.00 c/u","x2 $3200.00"]
     },
     {
-        nombre: "Petaca licor cusenier menta",
+        nombre: "Petaca licor cusenier menta[SIN DISPONIBILIDAD]",
         imagen: "imagenes/10.jpg",
         precio: "$2000.00"
     },
     {
-        nombre: "Galletita ''La providencia'' x3",
+        nombre: "Galletita ''La providencia'' x3[SIN DISPONIBILIDAD]",
         imagen: "imagenes/11.jpg",
         precio: "$1100.00"
     },
     {
-        nombre: "Papel higienico Duplex x50Mts",
+        nombre: "Papel higienico Duplex x50Mts[SIN DISPONIBILIDAD]",
         imagen: "imagenes/12.jpg",
         precio: "$600.00"
     },
     {
-        nombre: "Aceite cañuelas 1.5Lts",
+        nombre: "Aceite cañuelas 1.5Lts[SIN DISPONIBILIDAD]",
         imagen: "imagenes/13.jpg",
         precio: "$4200.00"
     },
     {
-        nombre: "Pure molto x520Grs",
+        nombre: "Pure molto x520Grs[SIN DISPONIBILIDAD]",
         imagen: "imagenes/14.jpg",
         precio: "$1000.00"
     },
     {
-        nombre: "Azucar marolio 1kg",
+        nombre: "Azucar marolio 1kg[SIN DISPONIBILIDAD]",
         imagen: "imagenes/16.jpg",
         precio: "$1100.00"
     },
     {
-        nombre: "Pan lactal ''Montesion'' x560Grs",
+        nombre: "Pan lactal ''Montesion'' x560Grs[SIN DISPONIBILIDAD]",
         imagen: "imagenes/17.jpg",
         precio: "$2500.00"
     },
     {
-        nombre: "Harina Marolio ''OOO'' 1Kg",
+        nombre: "Harina Marolio ''OOO'' 1Kg[SIN DISPONIBILIDAD]",
         imagen: "imagenes/18.jpg",
         precio: "$700.00"
     },
     {
-        nombre: "Rollo cocina ''Susex'' x3",
+        nombre: "Rollo cocina ''Susex'' x3[SIN DISPONIBILIDAD]",
         imagen: "imagenes/19.jpg",
         precio :"$1500.00"
     },
     {
-        nombre: "Lavavajillas Esencial x750cc",
+        nombre: "Lavavajillas Esencial x750cc[SIN DISPONIBILIDAD]",
         imagen: "imagenes/20.jpg",
         precio: "$1200.00"
     }
